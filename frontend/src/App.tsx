7e9766c1d94e2c228be7597c8d89ab9ec9c25316
GoodnessObi <goodnessobi@gmail.com>
2023-05-01 11:10:55 +0100

    chore: re-arrange project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './stylesheets/App.css';
-import FormView from './components/FormView';
-import QuestionView from './components/QuestionView.';
-import Header from './components/Header';
-import QuizView from './components/QuizView.';
+import FormView from './quiz/components/FormView';
+import QuestionView from './quiz/components/QuestionView.';
+import Header from './quiz/components/Header';
+import QuizView from './quiz/components/QuizView.';
 
 class App extends Component {
 	render() {
