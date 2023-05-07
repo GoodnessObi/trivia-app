0605e782208bb1cd78ff898ada61cdf83cf617df
GoodnessObi <goodnessobi@gmail.com>
2023-05-07 20:19:32 +0100

    feat: set up routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import './stylesheets/App.css';
 import QuestionList from './components/quiz/QuestionList.';
 import Header from './components/shared/Header';
@@ -7,25 +8,21 @@ import FormView from './components/questions/AddQuestion';
 import QuizView from './components/categories/QuizView.';
 import { QuizProvider } from './context/QuizProvider';
 
-class App extends Component {
-	render() {
-		return (
-			<div className='App'>
+const App = () => {
+	return (
+		<div className='App'>
+			<BrowserRouter>
 				<Header />
 				<QuizProvider>
-					<QuestionList />
-				</QuizProvider>
-				{/* <BrowserRouter>
 					<Routes>
 						<Route path='/' element={<QuestionList />} />
 						<Route path='/add' element={<FormView />} />
 						<Route path='/play' element={<QuizView />} />
-						<Route element={<QuestionList />} />
 					</Routes>
-				</BrowserRouter> */}
-			</div>
-		);
-	}
-}
+				</QuizProvider>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -1,45 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../stylesheets/Header.css';
+import { Link } from 'react-router-dom';
 
-class Header extends Component {
-	navTo(uri) {
-		window.location.href = window.location.origin + uri;
-	}
-
-	render() {
-		return (
-			<div className='App-header'>
-				<h1
-					onClick={() => {
-						this.navTo('');
-					}}
-				>
-					Udacitrivia
-				</h1>
-				<h2
-					onClick={() => {
-						this.navTo('');
-					}}
-				>
-					List
-				</h2>
-				<h2
-					onClick={() => {
-						this.navTo('/add');
-					}}
-				>
-					Add
-				</h2>
-				<h2
-					onClick={() => {
-						this.navTo('/play');
-					}}
-				>
-					Play
-				</h2>
-			</div>
-		);
-	}
+export default function Header() {
+	return (
+		<div className='App-header'>
+			<Link to='/'>Trivia</Link>
+			<Link to='/'>List</Link>
+			<Link to='/add'>Add</Link>
+			<Link to='/play'>Play</Link>
+		</div>
+	);
 }
-
-export default Header;
