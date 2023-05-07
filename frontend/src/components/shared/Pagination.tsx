91dec3ca7954ca216a7cb2c1c5db286e89e2d5ec
GoodnessObi <goodnessobi@gmail.com>
2023-05-07 20:03:49 +0100

    chore: install react-paginate

---
+++ frontend/src/components/shared/Pagination.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+export default function Pagination({ data }) {
+	// Here we use item offsets; we could also use page offsets
+	// following the API or data you're working with.
+	const [itemOffset, setItemOffset] = useState(0);
+	const itemsPerPage = 10;
+
+	// Simulate fetching items from another resources.
+	// (This could be items from props; or items loaded in a local state
+	// from an API endpoint with useEffect and useState)
+	const endOffset = itemOffset + itemsPerPage;
+	console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+	const currentItems = data.slice(itemOffset, endOffset);
+	const pageCount = Math.ceil(data.length / itemsPerPage);
+
+	// Invoke when user click to request another page.
+	const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % data.length;
+		console.log(
+			`User requested page number ${event.selected}, which is offset ${newOffset}`
+		);
+		setItemOffset(newOffset);
+	};
+	return (
+		<>
+			<ReactPaginate
+				breakLabel='...'
+				nextLabel='next >'
+				onPageChange={handlePageClick}
+				pageRangeDisplayed={5}
+				pageCount={pageCount}
+				previousLabel='< previous'
+				renderOnZeroPageCount={null}
+			/>
+		</>
+	);
+}
